5c1cb2ec3e35bfaf7cfc27a76e3919935052ad8c
Gustyx-Power <gustiadityamuzaky08@gmail.com>
2025-07-23 02:56:09 +0700

    #26 feat: Implement Easter Egg trigger logic
    This commit introduces the `EasterEggUtils` object to manage an easter egg feature.
    
    Key functionalities:
    - Tracks the number of clicks using `clickCount`.
    - Defines a `MAX_CLICKS` constant (set to 5) to trigger the easter egg.
    - `isEggTriggered` boolean state indicates if the easter egg has been activated.
    - `resetClickCount()` function resets the click counter and triggered state.
    - `onClick()` function increments the click count and sets `isEggTriggered` to true if `MAX_CLICKS` is reached, returning `true` in that case, and `false` otherwise.

--- app/src/main/java/id/xms/xtrakernelmanager/util/EasterEggUtils.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/util/EasterEggUtils.kt
@@ -1,4 +1,26 @@
 package id.xms.xtrakernelmanager.util
 
-class EasterEggUtils {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+object EasterEggUtils {
+    var clickCount by mutableStateOf(0)
+    const val MAX_CLICKS = 5
+    var isEggTriggered by mutableStateOf(false)
+
+    fun resetClickCount() {
+        clickCount = 0
+        isEggTriggered = false
+    }
+
+    fun onClick(): Boolean {
+        clickCount++
+        if (clickCount >= MAX_CLICKS) {
+            isEggTriggered = true
+            clickCount = 0
+            return true
+        }
+        return false
+    }
 }
