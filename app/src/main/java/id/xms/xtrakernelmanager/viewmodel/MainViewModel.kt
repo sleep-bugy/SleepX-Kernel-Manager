2dc23dc4e0a869b024b8da9af0334d46173fe874
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-29 07:38:45 +0700

    #62 feat: Implement MainViewModel
    This commit introduces the `MainViewModel` class.
    
    The `MainViewModel` is annotated with `@HiltViewModel` for Hilt dependency injection and extends `androidx.lifecycle.ViewModel`. An `@Inject` constructor is provided for Hilt to manage its instantiation.

--- app/src/main/java/id/xms/xtrakernelmanager/viewmodel/MainViewModel.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/viewmodel/MainViewModel.kt
@@ -1,4 +1,8 @@
 package id.xms.xtrakernelmanager.viewmodel
 
-class MainViewModel {
-}
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel()
