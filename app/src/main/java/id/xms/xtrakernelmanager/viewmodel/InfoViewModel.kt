e3fc27067299866a8efd3b1f17137afdc3b5a771
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-29 07:37:24 +0700

    #59 feat: Implement InfoViewModel
    This commit introduces the `InfoViewModel` class.
    
    The `InfoViewModel` is a Hilt ViewModel that retrieves system and kernel information using the `SystemRepository`. It exposes this information as `StateFlow`s:
    - `kernel`: Provides kernel information.
    - `system`: Provides system information.
    
    Both flows are initialized to emit data from the repository and are managed within the `viewModelScope`, remaining active as long as there are subscribers.

--- app/src/main/java/id/xms/xtrakernelmanager/viewmodel/InfoViewModel.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/viewmodel/InfoViewModel.kt
@@ -1,4 +1,20 @@
 package id.xms.xtrakernelmanager.viewmodel
 
-class InfoViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import id.xms.xtrakernelmanager.data.repository.SystemRepository
+import kotlinx.coroutines.flow.*
+import javax.inject.Inject
+
+@HiltViewModel
+class InfoViewModel @Inject constructor(
+    private val repo: SystemRepository
+) : ViewModel() {
+
+    val kernel = flow { emit(repo.getKernelInfo()) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+
+    val system = flow { emit(repo.getSystemInfo()) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
 }
