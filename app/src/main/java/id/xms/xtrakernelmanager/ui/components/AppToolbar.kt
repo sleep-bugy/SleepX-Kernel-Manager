0eb45c4f7cb0bc8e5f527b8cbbce082881f7c281
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-29 07:18:43 +0700

    #34 feat: Implement AppToolbar composable
    This commit introduces the `AppToolbar` composable function.
    
    The `AppToolbar` serves as a customizable top app bar. It displays a title and optionally a back navigation button.
    
    - If an `onBack` lambda is provided, it renders a `TopAppBar` with a back arrow icon that triggers the `onBack` function when clicked.
    - If `onBack` is not provided, it renders a `CenterAlignedTopAppBar` with only the title centered.
    
    The composable utilizes `ExperimentalMaterial3Api` components from Material 3.

--- app/src/main/java/id/xms/xtrakernelmanager/ui/components/AppToolbar.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/ui/components/AppToolbar.kt
@@ -1,4 +1,23 @@
 package id.xms.xtrakernelmanager.ui.components
 
-class AppToolbar {
+import android.graphics.drawable.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import org.w3c.dom.Text
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppToolbar(title: String, onBack: (() -> Unit)? = null) {
+    if (onBack != null) {
+        TopAppBar(title = { Text(title) },
+            navigationIcon = {
+                IconButton(onClick = onBack) {
+                    Icon(Icons.Default.ArrowBack, "back")
+                }
+            })
+    } else {
+        CenterAlignedTopAppBar(title = { Text(title) })
+    }
 }
