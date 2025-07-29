a1aae10ec658b3d156dcb1fd7ee4819a5fba7729
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-29 07:25:24 +0700

    #46 feat: Initialize RootRepository as Singleton
    This commit updates the `RootRepository` class by:
    - Importing `com.topjohnwu.superuser.Shell`.
    - Adding `@Inject` and `@Singleton` annotations.
    
    This change prepares the `RootRepository` for dependency injection and ensures it's treated as a singleton within the application.

--- app/src/main/java/id/xms/xtrakernelmanager/data/repository/RootRepository.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/data/repository/RootRepository.kt
@@ -1,4 +1,9 @@
 package id.xms.xtrakernelmanager.data.repository
 
 class RootRepository {
+import com.topjohnwu.superuser.Shell
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
 }
