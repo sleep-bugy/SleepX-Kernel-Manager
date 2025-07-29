e562eb94822f54c9d1c60e9978ba4974b2467d9d
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-29 07:28:09 +0700

    #47 feat: Implement RootRepository for root access and command execution
    This commit introduces the `RootRepository` class.
    
    The `RootRepository` is a Singleton class responsible for interacting with root privileges. It provides the following functionalities:
    - `isRooted()`: Checks if the device has root access using `Shell.getShell().isRoot`.
    - `run(cmd: String)`: Executes a given shell command with root privileges using `Shell.cmd(cmd).exec()` and returns the command's output.
    
    The class utilizes `com.topjohnwu.superuser.Shell` for its operations and is designed for dependency injection using `@Inject`.

--- app/src/main/java/id/xms/xtrakernelmanager/data/repository/RootRepository.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/data/repository/RootRepository.kt
@@ -1,9 +1,11 @@
 package id.xms.xtrakernelmanager.data.repository
 
-class RootRepository {
 import com.topjohnwu.superuser.Shell
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
+class RootRepository @Inject constructor() {
+    fun isRooted(): Boolean = Shell.getShell().isRoot
+    fun run(cmd: String): String = Shell.cmd(cmd).exec().out.joinToString("\n")
 }
