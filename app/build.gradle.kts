594230090326f53c0ae2c475642d725fe7a30869
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-30 23:17:01 +0700

    #84 build: Add WorkManager dependency
    This commit adds the `androidx.work:work-runtime-ktx:2.9.0` dependency to the project.
    This will enable the use of WorkManager for background task scheduling.
    
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
