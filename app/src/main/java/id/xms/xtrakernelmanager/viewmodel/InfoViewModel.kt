a84800e126310761d44ac622cc676db1d2e03fda
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-07-30 22:23:21 +0700

    #77 feat: Inject Context into InfoViewModel
    This commit modifies the `InfoViewModel` to include `Context` as a constructor dependency.
    
    The `Context` is provided using Hilt's `@ApplicationContext` qualifier. This change allows the ViewModel to access application-specific resources and system services if needed in the future.
    
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

--- app/src/main/java/id/xms/xtrakernelmanager/viewmodel/InfoViewModel.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/viewmodel/InfoViewModel.kt
@@ -1,14 +1,17 @@
 package id.xms.xtrakernelmanager.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import id.xms.xtrakernelmanager.data.repository.SystemRepository
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
 class InfoViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val repo: SystemRepository
 ) : ViewModel() {
 
