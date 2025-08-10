6e36cd6a04b6582a89535e20a2273e0358b830dd
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-08-10 07:57:42 +0700

    #165 feat: Display SoC name in RealtimeCpuInfo
    This commit adds a new field `soc` to the `RealtimeCpuInfo` data class.
    This field will store the name of the System on Chip (SoC) and defaults to "Unknown SOC".
    
    This change allows the `TuningViewModel` and related UI components to display the SoC information alongside other real-time CPU data.
    
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

--- app/src/main/java/id/xms/xtrakernelmanager/data/model/RealtimeCpuInfo.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/data/model/RealtimeCpuInfo.kt
@@ -1,6 +1,7 @@
 package id.xms.xtrakernelmanager.data.model
 
 import androidx.compose.runtime.Immutable
+import id.xms.xtrakernelmanager.viewmodel.TuningViewModel
 
 @Immutable
 data class RealtimeCpuInfo(
@@ -8,5 +9,6 @@ data class RealtimeCpuInfo(
     val governor: String,
     val freqs: List<Int>,   // MHz
     val temp: Float,        // °C
-    val cpuLoadPercentage: Float? = null
+    val cpuLoadPercentage: Float? = null,
+    val soc: String  = "Unknown SOC"
 )
