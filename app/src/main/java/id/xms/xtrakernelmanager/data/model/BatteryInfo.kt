68f9a09c9f997fbb10444906abe4a9c97017184f
Gusti Aditya Muzaky <142430633+gustyx-power@users.noreply.github.com>
2025-09-04 23:33:42 +0700

    feat(model): Enhance BatteryInfo model
    This commit enhances the `BatteryInfo` data class by:
    - Adding default values to all existing properties.
    - Introducing new properties: `voltage`, `isCharging`, `current`, `chargingWattage`, `technology`, `status`, and `chargingType`.
    - Changing the type of `health` from `Int` to `String`.
    - Removing the `@Serializable` annotation.
    - Removing `cycles` and `capacity` properties.
    
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

--- app/src/main/java/id/xms/xtrakernelmanager/data/model/BatteryInfo.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/data/model/BatteryInfo.kt
@@ -1,11 +1,14 @@
 package id.xms.xtrakernelmanager.data.model
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class BatteryInfo(
-    val level: Int,
-    val temp: Float,
-    val health: Int,
-    val cycles: Int,
-    val capacity: Int
+    val level: Int = 0,
+    val temp: Float = 0f,
+    val voltage: Float = 0f,
+    val isCharging: Boolean = false,
+    val current: Float = 0f,
+    val chargingWattage: Float = 0f,
+    val technology: String = "",
+    val health: String = "",
+    val status: String = "",
+    val chargingType: String = ""
 )
