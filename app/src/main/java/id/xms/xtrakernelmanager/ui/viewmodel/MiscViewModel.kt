1ab601537c0be56f3c36863258b7d9c01c0b25fc
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-09-04 23:34:37 +0700

    feat: Add MiscViewModel
    This commit introduces a new ViewModel class, `MiscViewModel`, to the `id.xms.xtrakernelmanager.ui.viewmodel` package.
    
    The `MiscViewModel` is annotated with `@HiltViewModel` for Hilt dependency injection.
    
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

---
+++ app/src/main/java/id/xms/xtrakernelmanager/ui/viewmodel/MiscViewModel.kt
@@ -0,0 +1,8 @@
+package id.xms.xtrakernelmanager.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MiscViewModel @Inject constructor() : ViewModel()
