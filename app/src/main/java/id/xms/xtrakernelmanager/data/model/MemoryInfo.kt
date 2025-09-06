f26dbfb4f7edf8255ba292d1b58fbf57f0736e63
gustyxpower <gustiadityamuzaky08@gmail.com>
2025-09-07 00:03:43 +0700

    feat(data): Add ZRAM and Swap details to MemoryInfo
    This commit enhances the `MemoryInfo` data model to include information about ZRAM and Swap.
    
    - Added `zramTotal` and `zramUsed` fields to track total and used ZRAM respectively.
    - Added `swapTotal` and `swapUsed` fields to track total and used Swap space respectively.
    
    These fields default to `0L`.
    
    Signed-off-by: gustyxpower <gustiadityamuzaky08@gmail.com>

--- app/src/main/java/id/xms/xtrakernelmanager/data/model/MemoryInfo.kt
+++ app/src/main/java/id/xms/xtrakernelmanager/data/model/MemoryInfo.kt
@@ -7,4 +7,8 @@ data class MemoryInfo(
     val used: Long,
     val total: Long,
     val free: Long,
+    val zramTotal: Long = 0L,
+    val zramUsed: Long = 0L,
+    val swapTotal: Long = 0L,
+    val swapUsed: Long = 0L,
 )
